fc288757124ec4afa4f1be2ca049440ad14b6dfd
Shubham Kumar Gupta <51732065+sshubham07@users.noreply.github.com>
2025-06-29 08:13:21 +0530

    Time: 1611 ms (18.84%), Space: 0B (100%) - LeetHub

---
+++ 0607-sales-person/0607-sales-person.sql
@@ -0,0 +1,3 @@
+select name from SalesPerson where name not in(
+    select s.name from SalesPerson s left join Orders o on s.sales_id=o.sales_id left join Company c on o.com_id=c.com_id where c.name="RED"
+);
