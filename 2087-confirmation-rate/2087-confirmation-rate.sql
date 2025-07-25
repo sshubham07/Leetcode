5f4cae7904fdabcec24ddea901ea460935cf9419
Shubham Kumar Gupta <51732065+sshubham07@users.noreply.github.com>
2025-07-25 23:04:47 +0530

    Time: 628 ms (71.59%), Space: 0B (100%) - LeetHub

--- 2087-confirmation-rate/2087-confirmation-rate.sql
+++ 2087-confirmation-rate/2087-confirmation-rate.sql
@@ -3,4 +3,4 @@ with cte as
 (
     select s.user_id, c.action from Signups s left join Confirmations c on s.user_id=c.user_id
 )
-select user_id, round(count(case when action='confirmed' then 1 end)/count(*),2) as confirmation_rate from cte group by user_id ;
+select user_id, round(count(case when action="confirmed" then 1 end)/count(*),2) as confirmation_rate from cte group by user_id ;
