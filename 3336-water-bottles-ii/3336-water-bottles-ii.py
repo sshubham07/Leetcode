99a1e03adc78e4fbea09e400964e1f85eb614d2c
Shubham Kumar Gupta <51732065+sshubham07@users.noreply.github.com>
2025-10-03 04:50:01 +0530

    Time: 28 ms (97.17%), Space: 17.4 MB (98.87%) - LeetHub

--- 3336-water-bottles-ii/3336-water-bottles-ii.py
+++ 3336-water-bottles-ii/3336-water-bottles-ii.py
@@ -1,16 +1,15 @@
 class Solution:
-    def calc(self, n,e,dp,empty):
-        if n==0 and empty<e:
-            return 0
-        if dp[n][e][empty]!=-1:
-            return dp[n][e][empty]
-        if empty>=e and n>0:
-            dp[n][e][empty]=max(1+self.calc(n-1,e,dp,empty+1),self.calc(n+1,e+1,dp,empty-e))
-        elif empty>=e:
-            dp[n][e][empty]=self.calc(n+1,e+1,dp,empty-e)
-        else:
-            dp[n][e][empty]=1+self.calc(n-1,e,dp,empty+1)
-        return dp[n][e][empty]
     def maxBottlesDrunk(self, n: int, e: int) -> int:
-        dp = [[[-1]*(n+e+2) for i in range(n+e+2)] for j in range(n+e+2)]
-        return self.calc(n,e,dp,0)
+        ans = 0
+        empty=0
+        while n>0 or empty>=e:
+            if n>0:
+                ans +=n
+                empty+=n
+                n=0
+            else:
+                ans+=1
+                empty-=e-1
+                e+=1
+        return ans
+        
